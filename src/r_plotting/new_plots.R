46e9de93e9e77df80d89179e0a67ea666189c332
Marcus Wilhelm <mail@marcuswilhelm.de>
2021-07-07 16:49:34 +0200

    less margin around new plot

--- src/r_plotting/new_plots.R
+++ src/r_plotting/new_plots.R
@@ -1,5 +1,7 @@
 source('helper.R')
+
 library("scales")
+library(tikzDevice)
 
 tbl <- read.csv('./new_output.csv')
 
@@ -7,19 +9,24 @@ tbl_sym <- read.csv('./symmetry.csv')
 
 tbl <- merge(tbl, tbl_sym, by = 'graph')
 
+tbl$algo_named <- replace(tbl$algo, tbl$algo=='tree-doubling', 'Generalized tree-doubling')
+tbl$algo_named <- replace(tbl$algo_named, tbl$algo_named=='christofides', 'Generalized Christofides')
+
 tbl$kernel_size_rel <- tbl$kernel_size / tbl$graph_size
 
 tbl_selection <- tbl[
   tbl$beta %in% c(1, 1.1, 2, 1.3, 1.5, 2) &
-  tbl$graph_size %in% c(100, 200, 300, 400, 500) &
+  #tbl$graph_size %in% c(100, 200, 300, 400, 500) &
   tbl$is_asymmetric == 1
   ,]
 
 #tbl_selection$kernel_size_rel <- tbl_selection$kernel_size_rel+0.0001
 
+
+
 p1 <- ggplot(tbl_selection,
              aes(
-               x = as.factor(graph_size),
+               x = cut_interval(graph_size,n=5),#as.factor(graph_size),
                y = kernel_size_rel,
                fill = as.factor(beta),
              )) +
@@ -29,14 +36,16 @@ p1 <- ggplot(tbl_selection,
     geom = "point",
     position = position_dodge(.9),
     shape = 5,
-    size = 4
+    size = 1.1
   ) +
-  scale_fill_discrete(name = "beta") +
+  scale_fill_discrete(name = "$\\beta$") +
+  scale_x_discrete(labels=c("50,\\\\100" ,"150,200", "250,300", "350,400", "450,500")) + 
   #  scale_y_continuous(trans=scales::pseudo_log_trans(sigma=0.001, base = 10)) + annotation_logticks(sides='l') +
   #  scale_y_log10() + annotation_logticks(sides='l') +
-  facet_wrap( ~ algo, ncol = 1, scales = 'free') +
+  facet_wrap( ~ algo_named, ncol = 2) + #, scales = 'free') +
   xlab("Graph size") +
-  ylab("Kernel size (relative)")
+  ylab("Parameter value (relative)") +
+  theme(legend.position = "bottom",legend.margin = margin(0,0,0,0),legend.box.margin = margin(0,0,0,0),plot.margin = margin(0,0,0,0))
 p1
 ggsave(
   "output.pdf/new_output1.pdf",
@@ -44,6 +53,10 @@ ggsave(
   width = 8,
   height = 10
 )
+tikz(file = "output.tex/kernel_size1.tex", 
+     width=4.8, height=3, base)
+print(p1)
+dev.off()
 
 p2 <- ggplot(tbl_selection,
              aes(
@@ -64,7 +77,8 @@ p2 <- ggplot(tbl_selection,
   #scale_y_log10() + annotation_logticks(sides='l') +
   facet_wrap( ~ algo, ncol = 1, scales = 'free') +
   xlab("Beta") +
-  ylab("Kernel size (relative)")
+  ylab("Kernel size (relative)") +
+  theme(legend.position = "bottom")
 p2
 ggsave(
   "output.pdf/new_output2.pdf",
@@ -72,3 +86,8 @@ ggsave(
   width = 8,
   height = 10
 )
+tikz(file = "output.tex/kernel_size2.tex", 
+     width=4.8, height=7.1, base)
+print(p2)
+dev.off()
+
